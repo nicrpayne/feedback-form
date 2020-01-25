202368dee0fbe2dff0cd8646b5cf427f74b480bc
Nic Payne <nic.r.payne@gmail.com>
2020-01-25 11:32:54 -0600

    initial router in, index drafted, server and client loaded

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
@@ -12,8 +15,9 @@ class App extends Component {
         </header>
         <br/>
       </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,51 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+
+// const pizzaSetter = (state = [], action) => {
+//     if (action.type === 'SET_PIZZA') {
+//         //whatever is returned will be saved in redux
+//         return action.payload
+//     }
+//     return state;
+// }
+
+// const deliveryType = (state = '', action) => {
+//     if (action.type === 'DELIVERYTYPE') {
+//         return action.payload
+//     }
+//     console.log('This is your delivery type', state)
+//     return state;
+// }
+
+// let addToCart = [];
+// addToCart.name
+
+// const addToCartReducer = (state = [], action) => {
+//     if (action.type === 'ADD_PRODUCT') {
+//         return [...state, action.payload]
+//     }
+//     return state;
+// }
+
+// const customerReducer = (state = {}, action) => {
+//     if (action.type === 'ADD_CUSTOMER') {
+//         return action.payload
+//     }
+//     console.log('This is your customer', state)
+//     return state;
+// }
+
+const storeInstance = createStore(
+    combineReducers({
+        
+    }),
+    applyMiddleware(logger),
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+
 registerServiceWorker();
